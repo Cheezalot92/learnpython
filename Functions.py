658e875a531e5eb9a119978561d71a6a4ac9081a
Brandon D McAbee <bdmm18@gmail.com>
2023-10-09 18:03:24 -0400

    2nd commit

---
+++ Functions.py
@@ -0,0 +1,29 @@
+def my_function():
+    print("Hello From My Function!")
+    
+my_function()
+
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
+    
+my_function_with_args("brandon", "wassup")
+
+
+def my_function():
+    print("Hello From My Function!")
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s, From My Function!, I wish you %s"%(username, greeting))
+
+def sum_two_numbers(a, b):
+    return a + b
+
+
+my_function()
+
+
+my_function_with_args("John Doe", "a great year!")
+
+
+x = sum_two_numbers(1,2)
